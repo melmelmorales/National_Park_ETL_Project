e6ecfad1ac66ba61e3c70bb2b170ae2ea2e6777d
Victoriascott-re <159197221+VictoriaScott-re@users.noreply.github.com>
2024-07-23 19:33:36 -0700

    updated app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,77 +1,93 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
-from flask_cors import CORS
-import seaborn as sns
-import pandas as pd
-import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
-from config import Config  # Import the Config class
+import folium
+from folium.plugins import MarkerCluster
+from dotenv import load_dotenv
+import os
+import logging
 
 app = Flask(__name__)
-CORS(app)
+load_dotenv()
 
-# MongoDB client setup using the Config class
-client = MongoClient(Config.MONGODB_URI)
-db = client.national_parks_db
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+
+# MongoDB connection
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client['national_parks_db']
+
+# Function to create a map with parks
+def create_map(parks):
+    m = folium.Map(location=[48.8566, -121.5059], zoom_start=4)
+    marker_cluster = MarkerCluster().add_to(m)
+
+    for park in parks:
+        popup_content = f"<strong>{park['park_name']}</strong><br>"
+        popup_content += f"<a href='{park['park_url']}' target='_blank'>Park Website</a><br>"
+        if park.get('free_park', 'False') == 'True':
+            popup_content += "Free Park<br>"
+        else:
+            if park.get('total_fees'):
+                popup_content += f"Total Fees: ${park['total_fees']}<br>"
+            if park.get('pass_fee'):
+                popup_content += f"Pass Fee: ${park['pass_fee']}<br>"
+            popup_content += f"<a href='{park.get('fees_url', '#')}' target='_blank'>Fees Info</a><br>"
+        popup_content += f"Summer Temp: {park.get('min_summer_temp')} - {park['max_summer_temp']} °F<br>"
+        popup_content += f"Winter Temp: {park.get('min_winter_temp')} - {park['max_winter_temp']} °F<br>"
+        popup_content += f"Spring Temp: {park.get('min_spring_temp')} - {park.get('max_spring_temp')} °F<br>"
+        popup_content += f"Fall Temp: {park.get('min_fall_temp')} - {park.get('max_fall_temp')} °F<br>"
+
+        folium.Marker(
+            location=[float(park['park_latitude']), float(park['park_longitude'])],
+            popup=folium.Popup(popup_content, max_width=300)
+        ).add_to(marker_cluster)
+
+    return m._repr_html_()
 
 @app.route('/')
-def home():
-    return "Welcome to the National Parks API!"
+def index():
+    # Fetch unique amenities and activities
+    amenities = list(db['Amenities_Collection'].distinct("amenity_name"))
+    activities = list(db['Activities_Collection'].distinct("activity_name"))
 
-# API endpoint to get amenities and activities
-@app.route('/api/amenities_activities', methods=['GET'])
-def get_amenities_activities():
-    amenities_activities = list(db.Activities_and_Amenities_Collection.find({}, {'_id': 0}))
-    return jsonify(amenities_activities)
+    # Fetch all parks for initial map display
+    all_parks = list(db['Activities_and_Amenities_Collection'].find({}, {"_id": 0}))
+    map_html = create_map(all_parks)
 
-# API endpoint to get entrance fees
-@app.route('/api/entrance_fees', methods=['GET'])
-def get_entrance_fees():
-    entrance_fees = list(db.Entrance_Fees_Collection.find({}, {'_id': 0}))
-    return jsonify(entrance_fees)
+    return render_template('index.html', amenities=amenities, activities=activities, map_html=map_html)
 
-# API endpoint to get things to do
-@app.route('/api/things_to_do', methods=['GET'])
-def get_things_to_do():
-    things_to_do = list(db.Things_to_Do_Collection.find({}, {'_id': 0}))
-    return jsonify(things_to_do)
-
-# API endpoint to get filtered parks based on user preferences
-@app.route('/api/filtered_parks', methods=['POST'])
-def get_filtered_parks():
+@app.route('/filter_parks', methods=['POST'])
+def filter_parks():
     data = request.json
-    amenities = data.get('amenities')
-    activities = data.get('activities')
-    season = data.get('season')
-    preferred_weather = data.get('preferred_weather')
+    selected_amenities = data.get('selected_amenities', [])
+    selected_activities = data.get('selected_activities', [])
+    selected_season = data.get('selected_season', 'summer')
 
-    # Perform filtering based on user preferences
+    # Determine temperature fields based on selected season
+    season_temp_fields = {
+        'summer': ('min_summer_temp', 'max_summer_temp'),
+        'winter': ('min_winter_temp', 'max_winter_temp'),
+        'spring': ('min_spring_temp', 'max_spring_temp'),
+        'fall': ('min_fall_temp', 'max_fall_temp')
+    }
+    min_temp_field, max_temp_field = season_temp_fields[selected_season]
+
     query = {}
-    if amenities:
-        query['amenity_name'] = {'$in': amenities}
-    if activities:
-        query['activity_name'] = {'$in': activities}
-    if season:
-        query['season'] = season
-    if preferred_weather:
-        query['min_summer_temp'] = {'$lte': preferred_weather['max_temp']}
-        query['max_summer_temp'] = {'$gte': preferred_weather['min_temp']}
+    if selected_amenities:
+        query['amenity_name'] = {'$in': selected_amenities}
+    if selected_activities:
+        query['activity_name'] = {'$in': selected_activities}
 
-    filtered_parks = list(db.Activities_and_Amenities_Collection.find(query, {'_id': 0}))
-    return jsonify(filtered_parks)
+    logging.debug(f"Filter Query: {query}")
 
-# API endpoint to get seaborn plot
-@app.route('/api/seaborn_plot')
-def seaborn_plot():
-    df = pd.DataFrame(list(db.Activities_and_Amenities_Collection.find({}, {'_id': 0})))
-    sns_plot = sns.scatterplot(x='min_summer_temp', y='max_summer_temp', data=df)
-    img = BytesIO()
-    sns_plot.figure.savefig(img, format='png')
-    img.seek(0)
-    plot_url = base64.b64encode(img.getvalue()).decode()
-    plt.close()
-    return render_template('seaborn_plot.html', plot_url=plot_url)
+    # Fetch parks matching the criteria
+    matching_parks = list(db['Activities_and_Amenities_Collection'].find(query, {"_id": 0}))
 
-if __name__ == '__main__':
+    logging.debug(f"Matching Parks: {matching_parks}")
+
+    # Create map with the filtered parks
+    map_html = create_map(matching_parks)
+    return jsonify({'map_html': map_html})
+
+if __name__ == "__main__":
     app.run(debug=True)
